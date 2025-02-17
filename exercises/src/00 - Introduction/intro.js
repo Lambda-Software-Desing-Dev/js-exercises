4671dba6c52124aac009159dfa2ce68da0ae87dc
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2025-02-16 21:30:03 -0300

    first exercise

--- exercises/src/00 - Introduction/intro.js
+++ exercises/src/00 - Introduction/intro.js
@@ -1,5 +1,23 @@
 export function callHtml(element) {
     element.innerHTML = `
-    <h2>Welcome to intro!</h2>
+    <h2>00 - Introduccion</h2>
+    <p>
+        En este capitulo realizaremos ejercicios basicos de Javascript. <br />
+        Comenzaremos declarando una variable, asignadole el valor 'Hola Mundo' <br />y mostrando el resultado por consola. <br />
+    
+        Si desea ver como resolver este ejercicio haga click en "Resolucion"
+        <details>
+            <summary>Resolucion</summary>
+            <pre>
+                let gretting = "Hola Mundo!"
+                console.log(gretting)
+            </pre>
+        </details>
+    </p>
+    <iframe src="https://www.jsplayground.dev/" title="jsplayground" width="640px" height="480px">
+    </iframe>
+
+    <p>Agradecimientos especiales a <a href="https://www.jsplayground.dev/">jsplayground.dev</a>
+    por facilitar este entorno</p>
     `
 }
--- exercises/src/main.js
+++ exercises/src/main.js
@@ -8,19 +8,25 @@ document.querySelector('#app').innerHTML = `
       <a id="logo">
         <img src="${lambdaLogo}" class="logo" alt="Lambda logo" />
       </a>
-      <h1>Welcome to Lambda-SD&D School!</h1>
+      <h1>Bienvenido a Lambda-SD&D School!</h1>
       <p class="read-the-docs">
-        Click on the logo to learn more
+        Haga click en el logo para continuar
       </p>
     </div>
-    <div id="main"></div>
+    <div id="main" style="display:none">
+      <div id="nav"></div>
+      <div id="content"></div>
+    </div>
   </div>
 `
 
 let link = document.querySelector("#logo")
+//document.querySelector("#nav").innerHTML = await (await fetch('./src/nav/nav.html')).text()
+
 link.addEventListener('click', (e) =>
 {
-  callHtml(document.querySelector('#main'))
+  callHtml(document.querySelector('#content'))
+  document.querySelector("#main").style.display = 'inline'
   document.querySelector("#home").style.display = 'none'
 })
 
