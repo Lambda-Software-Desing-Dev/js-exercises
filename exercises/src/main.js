91e9347ee2859bfe9b828df1fd08c9cd7c955105
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2025-02-19 10:30:18 -0300

    continue with 'menu'

--- exercises/src/main.js
+++ exercises/src/main.js
@@ -14,24 +14,28 @@ document.querySelector('#app').innerHTML = `
       </p>
     </div>
     <div id="main" style="display:none">
-      <div id="nav"></div>
-      <div id="content"></div>
+      <div id="menu" class="menu"></div>
+      <div id="nav" class="nav"></div>
+      <div id="content" class="content">
+      </div>
       <iframe src="https://www.jsplayground.dev/" title="jsplayground" width="640px" height="480px">
       </iframe>
-
-      <p>Agradecimientos especiales a <a href="https://www.jsplayground.dev/">jsplayground.dev</a>
-      por facilitar este entorno</p>
+      <div id="footer" class="footer">
+        <p>Agradecimientos especiales a <a href="https://www.jsplayground.dev/">jsplayground.dev</a>
+        por facilitar este entorno. Siganlos en sus redes :-)</p>
+      </div>
     </div>
   </div>
 `
 
 let logo = document.querySelector("#logo")
 document.querySelector("#nav").innerHTML = await (await fetch('./src/00 - Introduction/nav.html')).text()
+document.querySelector("#menu").innerHTML = await (await fetch('./src/menu.html')).text()
 
 logo.addEventListener('click', async (e) =>
 {
   await intro(document.querySelector('#content'), true)
-  document.querySelector("#main").style.display = 'inline'
+  document.querySelector("#main").style.display = 'grid'
   document.querySelector("#home").style.display = 'none'
 })
 
