678e9c85c59d8fef68ecba20e7f1562fe57a14a7
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2025-03-02 19:32:22 -0300

    test fixes

--- exercises/src/00.Introduction/intro.js
+++ exercises/src/00.Introduction/intro.js
@@ -4,7 +4,7 @@ import { exercise3 } from './exercise3'
 import { setMenu } from '../menu'
 
 export async function intro0(content, setNav) {
-    content.innerHTML = await (await fetch('./src/00.Introduction/intro0.html')).text()
+    content.innerHTML = await (await fetch('./00.Introduction/intro0.html')).text()
 
     if(setNav) {
         setNavigation(content)
@@ -13,8 +13,8 @@ export async function intro0(content, setNav) {
 
 async function setNavigation(content) {
     document.querySelector("#nav").innerHTML = await (await fetch('./00.Introduction/nav.html')).text()
-    document.querySelector("#menu").innerHTML = await (await fetch('./menu.html')).text()
-    setMenu()
+    //document.querySelector("#menu").innerHTML = await (await fetch('./menu.html')).text()
+    //setMenu()
 
     document.querySelector("#intro0").addEventListener('click', async () =>
     {
