cd96d4e7f3b17d44fbe98e77d83bbf8f228fb03e
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2025-03-04 21:16:36 -0300

    refactor chapter 2

--- exercises/src/02.Strings/string0.js
+++ exercises/src/02.Strings/string0.js
@@ -1,9 +1,3 @@
-export async function string0(content, setNav) {
-    /*content.innerHTML = await (await fetch('./src/00.Introduction/intro0.html')).text()
-
-    if(setNav) {
-        setNavigation(content)
-    }*/
-
-    content.innerHTML = '<h2>Esto es el Capitulo 02 - Manejo de Strings</h2><h3>Ejercicio 0</h3><p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>'
+export function string0Html() {
+    return `<h2>Esto es el Capitulo 02 - Manejo de Strings</h2><h3>Ejercicio 0</h3><p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>`
 }
--- exercises/src/menu.js
+++ exercises/src/menu.js
@@ -1,6 +1,6 @@
 import { intro0 } from './00.Introduction/intro'
 import { arith0 } from './01.Arithmetic/arith0'
-import { string0 } from './02.Strings/string0'
+import { string0Html } from './02.Strings/string0'
 
 export function menuHtml() {
     return  `
@@ -24,13 +24,13 @@ export function setMenu() {
         intro0(content, true)
     })
     
-    document.querySelector("#cap1").addEventListener('click', async function cap1()
+    document.querySelector("#cap1").addEventListener('click', () =>
     {
-        await arith0(content, true)
+        arith0(content, true)
     })
-    /*
-    document.querySelector("#cap2").addEventListener('click', async function cap2()
+    
+    document.querySelector("#cap2").addEventListener('click', () =>
     {
-        await string0(content, true)
-    })*/
+        content.innerHTML = string0Html()
+    })
 }
