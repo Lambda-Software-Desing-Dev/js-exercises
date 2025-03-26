f475b7d69c4fa6efae255c0334309c490d0114e2
Your Name <pgonzalezdeveloper@gmail.com>
2025-03-25 23:46:37 -0300

    remove commented code

--- exercises/src/01.Arithmetic/arith1.js
+++ exercises/src/01.Arithmetic/arith1.js
@@ -1,7 +1,3 @@
-/*export async function arith1(content) {
-    content.innerHTML = await (await fetch('./src/01 - Arithmetic/arith1.html')).text()
-}
-*/
 export function arith1Html() {
     return `
 <h2>Capitulo 01 - Aritmetica Basica</h2>
